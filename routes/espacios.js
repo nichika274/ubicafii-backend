11ad625ed60a26fe8845124f0a263fc42a3898f0
JORDY <nichika274@gmail.com>
2026-06-26 18:08:19 -0500

    Corregir rutas de espacios

--- routes/espacios.js
+++ routes/espacios.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const ctrl = require('../controllers/bloquesController');
+const ctrl = require('../controllers/espaciosController');
 
+// seed arriba para que no choque con /:id
 router.get('/seed', ctrl.seed);
 
 router.get('/', ctrl.getAll);
 
-router.get('/:bloque', ctrl.getByBloque);
+router.get('/:id', ctrl.getById);
+
+router.post('/', ctrl.create);
+
+router.put('/:id', ctrl.update);
+
+router.delete('/:id', ctrl.delete);
 
 module.exports = router;
