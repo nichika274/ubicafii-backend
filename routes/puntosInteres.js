fdf0d4c5d27faa60f70154274d889855d8e284a2
JORDY <nichika274@gmail.com>
2026-06-27 15:43:33 -0500

    Asegurar creación de tabla puntos_interes

--- routes/puntosInteres.js
+++ routes/puntosInteres.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const ctrl = require('../controllers/puntosInteresController');
 
 router.get('/', ctrl.getAll);
-router.post('/seed', ctrl.seed); // temporal
+router.post('/seed', ctrl.seed);   // <-- esta línea es clave
 
 module.exports = router;
